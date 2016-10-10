cfda359fcba8b4f052c241aace044ecb12961c13
Oscar Hall <oscar.hall@outlook.com>
2016-10-10 23:17:37 +0200

    Updating comments to doxyGen style

--- include/fileReader.h
+++ include/fileReader.h
@@ -4,31 +4,35 @@
 #include <string>
 #include <mutex>
 #include <map>
-#include <vector>
+#include <memory>
+#include "file.h"
 
 using namespace std;
 
 class fileReader
 {
     public:
-        fileReader();
-        ~fileReader();
+        fileReader() {}
+        ~fileReader() {}
 
         bool getFile(const string &filename, string &retFileContent, string &retContentType);
         bool setDirectory(const string &dir);
         string getDirectory();
 
+
     protected:
 
     private:
         string directory;
-        map<string,vector<string>> fileCache;
+        map<string, unique_ptr<fileObject>> fileCache;
 
         mutex dirMutex;
         mutex cacheMutex;
 
         bool bufferFile(string filename);
-        bool addFileToCache(const string &filename, const string &content);
+        bool addFileToCache(unique_ptr<fileObject> newFile);
+
+
 };
 
 #endif // FILEREADER_H
--- src/fileReader.cpp
+++ src/fileReader.cpp
@@ -1,40 +1,69 @@
 #include "fileReader.h"
 #include <fstream>
+#include <iostream>
 
-fileReader::fileReader()
-{
-    //ctor
-}
-
-fileReader::~fileReader()
-{
-    //dtor
-}
-
+/** \brief Buffers a file in server-directory to a new file object
+ *  and adds it to the fileCache
+ *
+ * \param string filename - File to buffer
+ * \return bool - True if file is successfully buffered, false o/w
+ *
+ */
 bool fileReader::bufferFile(string filename)
 {
     fstream file;
     file.exceptions( ifstream::failbit | ifstream::badbit );
 
     try{
+        string content;
         //Open file and read the file to string
-        file.open(this->getDirectory() + filename);
-        string tmp, readFromFile;
-
-        while(!file.eof()){
-            getline(file, tmp);
-            readFromFile += tmp + "\n";
+        if( filename.find(".jpg") != string::npos )
+        {
+            char tmpChar=0;
+            string tmpStr;
+            file.open(this->getDirectory() + filename, ios_base::in|ios_base::binary);
+            file.seekg(0);
+            while( file.read((char*) &tmpChar, sizeof(tmpChar)) )
+            {
+                tmpStr = tmpChar;
+                content.append(tmpStr);
+            }
+            file.close();
         }
-        this->addFileToCache(readFromFile);
+        else
+        {
+            file.open(this->getDirectory() + filename);
+            string tmp, readFromFile;
 
-        return true;
+            while(!file.eof())
+            {
+                getline(file, tmp);
+                readFromFile += tmp + "\n";
+            }
+            file.close();
+            content = readFromFile;
+        }
+
+        unique_ptr<fileObject> tmpPtr( new fileObject(filename, content, "text/html") );
+
+        if( this->addFileToCache(move(tmpPtr) ))
+            return true;
+
+        return false;
     }
-    catch(...){
+    catch(const ios_base::failure& e){
         cerr << "file not found..";
         return false;
     }
 }
 
+/** \brief Sets the working directory of the fileReader
+ *  and buffers the index.html file to the fileCache
+ *
+ * \param dir const string&
+ * \return bool - True if directory is successfully set and a index.html file is buffered, false o/w
+ *
+ */
 bool fileReader::setDirectory(const string &dir)
 {
     dirMutex.lock();
@@ -54,10 +83,71 @@ bool fileReader::setDirectory(const string &dir)
     return false;
 }
 
+/** \brief Returns current working directory
+ *
+ * \return string
+ *
+ */
 string fileReader::getDirectory()
 {
     dirMutex.lock();
     string tmp = directory;
     dirMutex.unlock();
     return tmp;
+}
+
+/** \brief Adds a file to the fileCache.
+ *
+ * \param newFile unique_ptr<fileObject> - Pointer to fileObject to be added
+ * \return bool - True if file is successfully added to cache, false o/w
+ *
+ */
+bool fileReader::addFileToCache(unique_ptr<fileObject> newFile)
+{
+    if(newFile)
+    {
+        cacheMutex.lock();
+        fileCache.insert( pair<string, unique_ptr<fileObject>>(newFile->getFilename(), move(newFile) ));
+        cacheMutex.unlock();
+        return true;
+    }
+    return false;
+}
+
+/** \brief Returns a requested file in working directory or a sub directory
+ *  specified by filename.
+ *
+ * \param filename const string& - Requested file
+ * \param retFileContent string& - Content of requested file if found
+ * \param retContentType string& - Content-type of requested file if found
+ * \return bool - True if requested file is found, false o/w
+ *
+ */
+bool fileReader::getFile(const string &filename, string &retFileContent, string &retContentType)
+{
+    if(!filename.empty())
+    {
+        bool fileInCache = false;
+        cacheMutex.lock();
+        auto it = fileCache.find(filename);
+        cacheMutex.unlock();
+
+        if( it != fileCache.end() )
+        {
+            fileInCache = true;
+        }
+        else if(bufferFile(filename))
+        {
+            fileInCache = true;
+        }
+        if(fileInCache)
+        {
+            cacheMutex.lock();
+            retFileContent = fileCache[filename]->getContent();
+            retContentType = fileCache[filename]->getContentType();
+            cacheMutex.unlock();
+            return true;
+        }
+    }
+    return false;
 }
