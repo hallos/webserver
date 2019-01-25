d03582fc138b8ac8efb88bd28423d57e47755d82
Oscar Hall <oscar.hall@outlook.com>
2019-01-25 23:58:23 +0100

    Handle request content-length

--- src/WebConnectionHandler.cpp
+++ src/WebConnectionHandler.cpp
@@ -4,6 +4,33 @@
 void WebConnectionHandler::onAccept(int id, std::shared_ptr<ITCPClientSocket> clientSocket)
 {
     std::string request = clientSocket->receiveData();
+    auto headerLength = request.find("\r\n\r\n");
+    if (headerLength == string::npos)
+    {
+        // Respond to bad request
+    }
+    std::string header = request.substr(0, headerLength);
+    std::string body = request.substr(headerLength+4, request.length());
+    // Interpret header
+    // Construct response if HEAD or GET request
+    // Check Expect header for 100-continue on POST request
+    // Fetch content-length and then body if request is a POST.
+    int contentLength = 0;
+    int receivedContent = body.length();
+    while (contentLength > receivedContent)
+    {
+        int bytesMissing = contentLength - receivedContent;
+        std::string data = clientSocket->receiveData();
+        if (data.length() > bytesMissing)
+        {
+            body.append(data, 0, bytesMissing);
+        }
+        else
+        {
+            body.append(data);
+        }
+    }
+/*
     httpInterpreter interpreter(request);
     std::string requestedFile;
     if (interpreter.interpretRequest(requestedFile))
@@ -16,4 +43,5 @@ void WebConnectionHandler::onAccept(int id, std::shared_ptr<ITCPClientSocket> cl
         std::string response = interpreter.getResponse();
         clientSocket->sendData(response);
     }
+    */
 }
