4c7fd13e18a74b0d3d934f6495659a7c87b066c0
Oscar Hall <oscar.hall@outlook.com>
2019-04-22 20:15:07 +0200

    Remove ui class and run application without console ui

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,13 @@
-#include <thread>
 #include <vector>
 #include "HTTP.h"
 #include "Server.h"
 #include <FileReader.h>
-#include "ui.h"
 #include "ctpl_stl.h"
 #include "WebConnectionHandler.h"
 
 
 int main()
 {
-    system("title webServer"); //Set title of command-prompt
-    std::vector<std::unique_ptr<std::thread>> Threads;
     auto threadPool = std::make_shared<ctpl::thread_pool>(4);
     // TODO: Read root dir and port from config file
     std::string rootDir = "/home/oscar/Documents/Projekt/hello_web";
@@ -21,29 +17,7 @@ int main()
     auto connectionHandler = std::make_shared<WebConnectionHandler>(fileReader);
     Server webserver(threadPool, serverSocket, connectionHandler);
     
-    bool exit = false;
-    do{
-        printMainMenu(webserver.isRunning());
-        switch(getMenuOption(3))
-        {
-        case 1:
-            Threads.emplace_back(new std::thread(&Server::startServer,&webserver));
-            break;
-        case 2:
-            webserver.stopServer();
-            break;
-        case 0:
-            webserver.stopServer();
-            exit=true;
-            break;
-        default:
-            break;
-        }
+    webserver.startServer();
 
-    }while(!exit);
-    for (auto& t : Threads)
-    {
-        t->join();
-    }
     return 0;
 }
