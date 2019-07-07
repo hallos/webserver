cde1c5270967e97130322a8fbb30024d4ae94be8
Oscar Hall <oscar.hall@outlook.com>
2019-07-07 15:25:15 +0200

    Update build and usage instructions

--- src/main.cpp
+++ src/main.cpp
@@ -10,24 +10,40 @@
 int main(int argc, char** argv)
 {
     int port = 8080;
+    int nrThreads = 4;
     std::string rootDirectory = "test/system";
-    auto cli = clara::Opt(port, "port")
+    bool showHelp = false;
+    auto cli = clara::Help(showHelp)
+             | clara::Opt(port, "port")
                 ["-p"]["--port"]
                 ("Port to be used by webserver to receive connections." )
-             | clara::Opt(rootDirectory, "rootDirectory")
+             | clara::Opt(rootDirectory, "directorypath")
                 ["-d"]["--root-directory"]
-                ("Root directory of webserver. (Where to find index.html)" );
+                ("Root directory of webserver. (Where to find index.html)" )
+             | clara::Opt(nrThreads, "threadcount")
+                ["-t"]["--number-threads"]
+                ("Number if threads in internal threadpool serving incoming requests." );
     auto result = cli.parse(clara::Args(argc, argv));
     if (!result) 
     {
         std::cerr << "Error in command line: " << result.errorMessage() << std::endl;
         exit(1);
     }
+    else if (showHelp)
+    {
+        cli.writeToStream(std::cout);
+        return 0;
+    }
+    // Validate input data
+    if (nrThreads < 1)
+    {
+        nrThreads = 1;
+    }
 
     auto serverSocket = std::make_shared<TCPServerSocket>(port);
     auto fileReader = std::make_shared<FileReader>(rootDirectory);
     auto connectionHandler = std::make_shared<WebConnectionHandler>(fileReader);
-    Server webserver(serverSocket, connectionHandler, 4);
+    Server webserver(serverSocket, connectionHandler, nrThreads);
     webserver.startServer();
 
     while (true)
